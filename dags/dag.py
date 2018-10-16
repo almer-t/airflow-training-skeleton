b1e98bf33d7204bbc914ce7acccfcf8db509193a
Almer S. Tigelaar <almer@tigelaar.net>
2018-10-16 12:01:44 +0200

    * Change dag name.

--- dags/dag.py
+++ dags/dag.py
@@ -6,14 +6,14 @@ from godatadriven.operators.postgres_to_gcs import PostgresToGoogleCloudStorageO
 
 
 dag = DAG(
-    dag_id="my_first_dag",
+    dag_id="my_second_dag",
     schedule_interval="30 7 * * *",
     default_args={
         "owner": "airflow",
         "start_date": dt.datetime(2018, 8, 1),
         "depends_on_past": True,
         "email_on_failure": True,
-        "email": "airflow_errors@myorganisation.com",
+        "email": "atigelaar@bol.com",
     },
 )
 
