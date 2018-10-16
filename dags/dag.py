7ac77a59cc6c022d787ce7a7d6def21989c1e0aa
Almer S. Tigelaar <almer@tigelaar.net>
2018-10-16 15:55:40 +0200

    * parquet file spec added.

--- dags/dag.py
+++ dags/dag.py
@@ -58,7 +58,7 @@ with my_second_dag as dag:
     gcs_to_bq_task = GoogleCloudStorageToBigQueryOperator(
         task_id="gcs_to_bq_import",
         bucket="europe-west1-training-airfl-d9a9700f-data",
-        source_objects=["average_prices/transfer_date={{ ds }}/"],
+        source_objects=["average_prices/transfer_date={{ ds }}/*.parquet"],
         destination_project_dataset_table=table,
         source_format="PARQUET",
         write_disposition="WRITE_TRUNCATE")
