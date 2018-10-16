c010e30dcb16689538280caeba11f34cd9e8b157
Almer S. Tigelaar <almer@tigelaar.net>
2018-10-16 15:15:45 +0200

    * Fix long line.

--- dags/dag.py
+++ dags/dag.py
@@ -54,12 +54,12 @@ with my_second_dag as dag:
         project_id=project_id,
         trigger_rule=TriggerRule.ALL_DONE)
 
+    table = "airflowbolcom-656e0a307aa4039f:airflow_train_a.land_registry_${{ ds_nodash }}"
     gcs_to_bq_task = GoogleCloudStorageToBigQueryOperator(
         task_id="gcs_to_bq_import",
         bucket="europe-west1-training-airfl-d9a9700f-data",
         source_object="average_prices/transfer_date={{ ds }}/",
-        destination_project_dataset_table=\
-        "airflowbolcom-656e0a307aa4039f:airflow_train_a.land_registry_${{ ds_nodash }}",
+        destination_project_dataset_table=table,
         source_format="parquet",
         write_disposition="OVERWRITE")
 
