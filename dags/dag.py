3c70aac48cbaa369715da9a4d37e690bff3228f0
Almer S. Tigelaar <almer@tigelaar.net>
2018-10-16 13:49:06 +0200

    * Add dataproc stuff

--- dags/dag.py
+++ dags/dag.py
@@ -1,11 +1,16 @@
 import datetime as dt
 
 from airflow import DAG
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.contrib.operators.dataproc_operator import (
+    DataprocClusterCreateOperator,
+    DataprocClusterDeleteOperator,
+    DataProcPySparkOperator,
+)
 
 from godatadriven.operators.postgres_to_gcs import PostgresToGoogleCloudStorageOperator
 
-
-dag = DAG(
+my_second_dag = DAG(
     dag_id="my_second_dag",
     schedule_interval="30 7 * * *",
     default_args={
@@ -17,11 +22,33 @@ dag = DAG(
     },
 )
 
-psql_to_gcs = PostgresToGoogleCloudStorageOperator(
-    task_id="read_postgres_data_to_bucket",
-    postgres_conn_id="postgres_gcp",
-    sql="select * from land_registry_price_paid_uk where transfer_date = '{{ ds }}'",
-    bucket="europe-west1-training-airfl-d9a9700f-data",
-    filename="data/{{ ds }}-psql-land-registry-data.json",
-    dag=dag,
-)
+with my_second_dag as dag:
+    psql_to_gcs = PostgresToGoogleCloudStorageOperator(
+        task_id="read_postgres_data_to_bucket",
+        postgres_conn_id="postgres_gcp",
+        sql="select * from land_registry_price_paid_uk where transfer_date = '{{ ds }}'",
+        bucket="europe-west1-training-airfl-d9a9700f-data",
+        filename="data/{{ ds }}-psql-land-registry-data.json")
+
+    cluster_name = "my-dataproc-cluster-{{ ds }}"
+    project_id = "airflowbolcom-656e0a307aa4039f"
+    zone = "europe-west1"
+
+    dataproc_create_cluster = DataprocClusterCreateOperator(
+        task_id="dataproc_cluster_spinup",
+        cluster_name=cluster_name,
+        project_id=project_id,
+        num_workers=2,
+        zone=zone)
+
+    compute_aggregates = DataProcPySparkOperator(
+        task_id="dataproc_compute_aggs",
+        main="gs://europe-west1-training-airfl-d9a9700f-bucket/other/build_statistics.py",
+        cluster_name=cluster_name,
+        arguments=["{{ ds }}"])
+
+    dataproc_delete_cluster = DataProcClusterDeleteOperator(
+        task_id="dataproc_cluster_spindown",
+        cluster_name=cluster_name,
+        project_id=project_id,
+        trigger_rule=TriggerRule.ALL_DONE)
