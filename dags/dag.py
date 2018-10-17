279d9223a0d210d3f71107a9399fc4d7e9eeb30c
Almer S. Tigelaar <almer@tigelaar.net>
2018-10-17 13:32:17 +0200

    * Change to new assignment, read stuff into bq

--- dags/dag.py
+++ dags/dag.py
@@ -14,71 +14,86 @@ from godatadriven.operators.postgres_to_gcs import PostgresToGoogleCloudStorageO
 from http_to_gcs_operator import HttpToGcsOperator
 
 my_second_dag = DAG(
-    dag_id="my_second_dag",
+    dag_id="my_third_dag",
     schedule_interval="30 7 * * *",
     default_args={
         "owner": "airflow",
-        "start_date": dt.datetime(2018, 10, 1),
+        "start_date": dt.datetime(2018, 10, 8),
         "depends_on_past": True,
-        "email_on_failure": True,
-        "email": "atigelaar@bol.com",
+        "email_on_failure": False
     },
 )
 
 with my_second_dag as dag:
+    json_output_file = 'data/{{ ds }}-psql-land-registry-data.json'
+
     psql_to_gcs = PostgresToGoogleCloudStorageOperator(
         task_id="read_postgres_data_to_bucket",
         postgres_conn_id="postgres_gcp",
         sql="select * from land_registry_price_paid_uk where transfer_date = '{{ ds }}'",
         bucket="europe-west1-training-airfl-d9a9700f-data",
-        filename="data/{{ ds }}-psql-land-registry-data.json")
+        filename=json_output_file)
 
-    cluster_name = "my-dataproc-cluster-{{ ds }}"
-    project_id = "airflowbolcom-656e0a307aa4039f"
-    zone = "europe-west4-a"
+    import_into_bigquery = DataFlowPythonOperator(
+        task_id="import_bigquery",
+        dataflow_default_options={
+            'project': 'airflowbolcom-656e0a307aa4039f',
+            'zone': 'europe-west4',
+            'bucket': 'europe-west1-training-airfl-d9a9700f-dataflow',
+            'input': 'gs://{}/{}'.format('europe-west1-training-airfl-d9a9700f-data', json_output_file),
+            'table': 'airflowbolcom-656e0a307aa4039f:airflowtrain_b.{{ ds_nodash }}_full',
+            'name': 'df-{{ ds }}-import'
+        },
+        py_file="gs://airflow-training-data/dataflow_job.py")
 
-    dataproc_create_cluster = DataprocClusterCreateOperator(
-        task_id="dataproc_cluster_spinup",
-        cluster_name=cluster_name,
-        project_id=project_id,
-        num_workers=2,
-        zone=zone)
+    psql_to_gcs >> import_into_bigquery
 
-    compute_aggregates = DataProcPySparkOperator(
-        task_id="dataproc_compute_aggs",
-        main="gs://europe-west1-training-airfl-d9a9700f-bucket/other/build_statistics_simple.py",
-        cluster_name=cluster_name,
-        arguments=["{{ ds }}"])
+    # cluster_name = "my-dataproc-cluster-{{ ds }}"
+    # project_id = "airflowbolcom-656e0a307aa4039f"
+    # zone = "europe-west4-a"
 
-    dataproc_delete_cluster = DataprocClusterDeleteOperator(
-        task_id="dataproc_cluster_spindown",
-        cluster_name=cluster_name,
-        project_id=project_id,
-        trigger_rule=TriggerRule.ALL_DONE)
+    # dataproc_create_cluster = DataprocClusterCreateOperator(
+    #     task_id="dataproc_cluster_spinup",
+    #     cluster_name=cluster_name,
+    #     project_id=project_id,
+    #     num_workers=2,
+    #     zone=zone)
 
-    table = "airflowtrain_a.{{ ds_nodash }}_landregistry"
-    gcs_to_bq_task = GoogleCloudStorageToBigQueryOperator(
-        task_id="gcs_to_bq_import",
-        bucket="europe-west1-training-airfl-d9a9700f-data",
-        source_objects=["average_prices/transfer_date={{ ds }}/*.parquet"],
-        destination_project_dataset_table=table,
-        source_format="PARQUET",
-        write_disposition="WRITE_TRUNCATE")
+    # compute_aggregates = DataProcPySparkOperator(
+    #     task_id="dataproc_compute_aggs",
+    #     main="gs://europe-west1-training-airfl-d9a9700f-bucket/other/build_statistics_simple.py",
+    #     cluster_name=cluster_name,
+    #     arguments=["{{ ds }}"])
+
+    # dataproc_delete_cluster = DataprocClusterDeleteOperator(
+    #     task_id="dataproc_cluster_spindown",
+    #     cluster_name=cluster_name,
+    #     project_id=project_id,
+    #     trigger_rule=TriggerRule.ALL_DONE)
+
+    # table = "airflowtrain_a.{{ ds_nodash }}_landregistry"
+    # gcs_to_bq_task = GoogleCloudStorageToBigQueryOperator(
+    #     task_id="gcs_to_bq_import",
+    #     bucket="europe-west1-training-airfl-d9a9700f-data",
+    #     source_objects=["average_prices/transfer_date={{ ds }}/*.parquet"],
+    #     destination_project_dataset_table=table,
+    #     source_format="PARQUET",
+    #     write_disposition="WRITE_TRUNCATE")
 
     #
     # transform_currency_task = PythonOperator(
     #     task_id='test', python_callable=transform_currency,
     #     op_kwargs={'base_dir': base_dir}, dag=dag)
 
-    psql_to_gcs >> dataproc_create_cluster >> compute_aggregates >> dataproc_delete_cluster
-    gcs_to_bq_task.set_upstream(compute_aggregates)
+    # psql_to_gcs >> dataproc_create_cluster >> compute_aggregates >> dataproc_delete_cluster
+    # gcs_to_bq_task.set_upstream(compute_aggregates)
 
-    for currency in [ "EUR", "USD" ]:
-        currency_transform_task = HttpToGcsOperator(
-            task_id="get_currency_" + currency,
-            http_conn_id="http_default",
-            gcs_conn_id="gcs_default",
-            endpoint="https://europe-west1-gdd-airflow-training.cloudfunctions.net/airflow-training-transform-valutas?date={{ ds }}&from=GBP&to=EUR",
-            gcs_path="gs://europe-west1-training-airfl-d9a9700f-data/currencies/{{ ds }}-currency-" + currency + ".json"
-        )
-        compute_aggregates >> currency_transform_task
+    # for currency in [ "EUR", "USD" ]:
+    #     currency_transform_task = HttpToGcsOperator(
+    #         task_id="get_currency_" + currency,
+    #         http_conn_id="http_default",
+    #         gcs_conn_id="gcs_default",
+    #         endpoint="https://europe-west1-gdd-airflow-training.cloudfunctions.net/airflow-training-transform-valutas?date={{ ds }}&from=GBP&to=EUR",
+    #         gcs_path="gs://europe-west1-training-airfl-d9a9700f-data/currencies/{{ ds }}-currency-" + currency + ".json"
+    #     )
+    #     compute_aggregates >> currency_transform_task
