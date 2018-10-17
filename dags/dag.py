8d5fc6c08e662067f905da1bb7ef48e11cdff5ee
Almer S. Tigelaar <almer@tigelaar.net>
2018-10-17 11:59:14 +0200

    * Fix missing comma.

--- dags/dag.py
+++ dags/dag.py
@@ -67,7 +67,7 @@ with my_second_dag as dag:
 
     for currency in [ "EUR", "USD" ]:
         currency_transform_task = HttpToGcsOperator(
-            task_id="get_currency_" + currency
+            task_id="get_currency_" + currency,
             http_conn_id="http_default",
             endpoint="https://europe-west1-gdd-airflow-training.cloudfunctions.net/airflow-training-transform-valutas?date={{ ds }}&from=GBP&to=EUR",
             gcs_path="gs://europe-west1-training-airfl-d9a9700f-data/currencies/{{ ds }}-currency-{}.json".format(currency)
